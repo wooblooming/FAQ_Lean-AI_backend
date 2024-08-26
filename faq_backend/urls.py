51a078984aea6746f7adf80f6ab508a0db20c134
hoyoung94 <hoyoung940816@gmail.com>
2024-08-26 10:59:45 +0900

    08/26업주정보 수정기능 완성

--- faq_backend/urls.py
+++ faq_backend/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('faq.urls')),  # FAQ 앱 관련 API
     path('chatbot/', include('chatbot.urls')),  # 챗봇 앱 관련 API
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
